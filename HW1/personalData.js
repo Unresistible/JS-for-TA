36cd5edb205829a1acf1dc22c2e7b742d64b47c0
Unresistible <flaps@ua.fm>
2020-11-12 21:41:17 +0200

    homework1

---
+++ HW1/personalData.js
@@ -0,0 +1,11 @@
+let name = 'Vova', surname = 'Matsak', studyGroup = 'TA 2020', birthYear = 1989;
+let maritalStatus = false;
+console.log(birthYear);
+console.log(maritalStatus);
+console.log(name);
+console.log(surname);
+console.log(studyGroup);
+
+let randomUndef = undefined, randomNull = null;
+console.log(typeof(randomUndef));
+console.log(typeof(randomNull));
