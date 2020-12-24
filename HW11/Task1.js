d676cde88d81f547dd2cffcfad95f3f07c498e52
Unresistible <flaps@ua.fm>
2020-12-24 10:19:30 +0200

    add System information line

--- HW11/Task1.js
+++ HW11/Task1.js
@@ -2,12 +2,13 @@ const http = require('http');
 const os = require('os');
 const path = require('path');
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Current user name: '+ os.userInfo().username +'\n');
-  response.write('OS type: '+ os.type() +'\n');
-  response.write('System work time: '+ (os.uptime() / 60).toFixed(2) +' minutes\n');
-  response.write('Current work directory: '+ __dirname +'\n');
-  response.end('Server file name: '+ path.basename(__filename) +'\n');
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<h2>System information</h2>\n');
+  response.write('Current user name: '+ os.userInfo().username +'<br>');
+  response.write('OS type: '+ os.type() +'<br>');
+  response.write('System work time: '+ (os.uptime() / 60).toFixed(2) +' minutes<br>');
+  response.write('Current work directory: '+ __dirname +'<br>');
+  response.end('Server file name: '+ path.basename(__filename) +'<br>');
 }).listen(5000);
 
 console.log('Server running at http://127.0.0.1:5000/');
