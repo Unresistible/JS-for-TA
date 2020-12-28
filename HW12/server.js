3fba8c6a1dc84d867fc37c629e7d4c693f4ea488
Unresistible <flaps@ua.fm>
2020-12-28 16:02:31 +0200

    HW12 done

--- HW12/server.js
+++ HW12/server.js
@@ -1,15 +1,33 @@
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 const http = require('http');
-const url = require('url');
-const static = require('node-static');
-const file = new static.Server('.');
+var url = require('url');
+var static = require('node-static');
+var file = new static.Server('.', {
+    cache: 0
+});
+
 function accept(req, res) {
-    // if (req == 'books.json') {
-    //     file.serve(req, res);		// can set delay
-    // } else
-    if (req.url == 'data') {
-        res = 'asd';
+    //console.log('asd + asd: ' + req);
+    if (req.url == '/vote'){
+        console.log('asd + asd: ');
+        res.end('You vote is accepted: ' + new Date());
     }
+    else
+        {
+            console.log('asd2 + asd2: ');
+            file.serve(req, res);
+        }
+
+    if (req.url == '/books.json') {
+        setTimeout(function() {
+            file.serve(req, res);
+        }, 2000)}
+        else
+        {
+            file.serve(req, res);
+        }
 }
-// ------ run server -------
+
+// ------ run server -------
 http.createServer(accept).listen(8080);
-console.log("Server running on port 8080");
+console.log("Server running on http://localhost:8080");
